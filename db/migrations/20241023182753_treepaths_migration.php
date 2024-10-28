325ba862d99852d15116583a8e0be0e3bfd8197f
David Durieux <david@durieux.family>
2024-10-28 07:07:50 +0100

    Fix migration of treepath (tables in tree)

--- db/migrations/20241023182753_treepaths_migration.php
+++ db/migrations/20241023182753_treepaths_migration.php
@@ -8,154 +8,307 @@ use Phinx\Migration\AbstractMigration;
 use Phinx\Migration\Manager\Environment;
 use Phinx\Config\Config;
 use App\v1\Controllers\Toolbox;
+use Phinx\Db\Adapter\MysqlAdapter;
 
 final class TreepathsMigration extends AbstractMigration
 {
   protected $treepaths = [];
 
-  public function change(): void
+  public function up()
   {
     // we set fields and recalculate treepath for items in tree
     $table = $this->table('entities');
-    $table->addColumn('treepath', 'string', ['null' => true])
-          ->update();
-
-    $table = $this->table('categories');
-    $table->addColumn('treepath', 'string', ['null' => true])
-          ->update();
-
-    $table = $this->table('groups');
-    $table->addColumn('treepath', 'string', ['null' => true])
-          ->update();
-
-    $table = $this->table('knowbaseitemcategories');
-    $table->addColumn('treepath', 'string', ['null' => true])
-          ->update();
-
-    $table = $this->table('locations');
-    $table->addColumn('treepath', 'string', ['null' => true])
-          ->update();
-
-    $table = $this->table('softwarelicensetypes');
-    $table->addColumn('treepath', 'string', ['null' => true])
-          ->update();
-
-    $table = $this->table('softwarecategories');
-    $table->addColumn('treepath', 'string', ['null' => true])
-          ->update();
-
-    $table = $this->table('states');
-    $table->addColumn('treepath', 'string', ['null' => true])
-          ->update();
-
-    $table = $this->table('businesscriticities');
-    $table->addColumn('treepath', 'string', ['null' => true])
-          ->update();
-
-    $table = $this->table('documentcategories');
-    $table->addColumn('treepath', 'string', ['null' => true])
-          ->update();
-
-    $table = $this->table('ipnetworks');
-    $table->addColumn('treepath', 'string', ['null' => true])
-          ->update();
-
-    if ($this->isMigratingUp())
+    if (!$table->hasColumn('treepath'))
     {
-      $this->treepaths = [];
-      $this->generateTreepath('-1', 'entities', 'entity_id');
-
-      $this->treepaths = [];
-      $this->generateTreepath('0', 'categories', 'category_id');
-
-      $this->treepaths = [];
-      $this->generateTreepath('0', 'groups', 'group_id');
-
-      $this->treepaths = [];
-      $this->generateTreepath('0', 'knowbaseitemcategories', 'knowbaseitemcategory_id');
-
-      $this->treepaths = [];
-      $this->generateTreepath('0', 'locations', 'location_id');
-
-      $this->treepaths = [];
-      $this->generateTreepath('0', 'softwarelicensetypes', 'softwarelicensetype_id');
-
-      $this->treepaths = [];
-      $this->generateTreepath('0', 'softwarecategories', 'softwarecategory_id');
-
-      $this->treepaths = [];
-      $this->generateTreepath('0', 'states', 'state_id');
-
-      $this->treepaths = [];
-      $this->generateTreepath('0', 'businesscriticities', 'businesscriticity_id');
-
-      $this->treepaths = [];
-      $this->generateTreepath('0', 'documentcategories', 'documentcategory_id');
-
-      $this->treepaths = [];
-      $this->generateTreepath('0', 'ipnetworks', 'ipnetwork_id');
+      $table->addColumn('treepath', 'string', ['null' => true])
+      ->update();
     }
 
-
-    $table = $this->table('entities');
-    $table->removeColumn('level')
-          ->removeColumn('sons_cache')
-          ->removeColumn('ancestors_cache')
-          ->update();
+    $table = $this->table('categories');
+    if (!$table->hasColumn('treepath'))
+    {
+      $table->addColumn('treepath', 'string', ['null' => true])
+            ->update();
+    }
 
     $table = $this->table('groups');
-    $table->removeColumn('level')
-          ->removeColumn('sons_cache')
-          ->removeColumn('ancestors_cache')
-          ->update();
+    if (!$table->hasColumn('treepath'))
+    {
+      $table->addColumn('treepath', 'string', ['null' => true])
+            ->update();
+    }
 
     $table = $this->table('knowbaseitemcategories');
-    $table->removeColumn('level')
-          ->removeColumn('sons_cache')
-          ->removeColumn('ancestors_cache')
-          ->update();
+    if (!$table->hasColumn('treepath'))
+    {
+      $table->addColumn('treepath', 'string', ['null' => true])
+            ->update();
+    }
 
     $table = $this->table('locations');
-    $table->removeColumn('level')
-          ->removeColumn('sons_cache')
-          ->removeColumn('ancestors_cache')
-          ->update();
+    if (!$table->hasColumn('treepath'))
+    {
+      $table->addColumn('treepath', 'string', ['null' => true])
+            ->update();
+    }
 
     $table = $this->table('softwarelicensetypes');
-    $table->removeColumn('level')
-          ->removeColumn('sons_cache')
-          ->removeColumn('ancestors_cache')
-          ->update();
+    if (!$table->hasColumn('treepath'))
+    {
+      $table->addColumn('treepath', 'string', ['null' => true])
+            ->update();
+    }
 
     $table = $this->table('softwarecategories');
-    $table->removeColumn('level')
-          ->removeColumn('sons_cache')
-          ->removeColumn('ancestors_cache')
-          ->update();
+    if (!$table->hasColumn('treepath'))
+    {
+      $table->addColumn('treepath', 'string', ['null' => true])
+            ->update();
+    }
 
     $table = $this->table('states');
-    $table->removeColumn('level')
-          ->removeColumn('sons_cache')
-          ->removeColumn('ancestors_cache')
-          ->update();
+    if (!$table->hasColumn('treepath'))
+    {
+      $table->addColumn('treepath', 'string', ['null' => true])
+            ->update();
+    }
 
     $table = $this->table('businesscriticities');
-    $table->removeColumn('level')
-          ->removeColumn('sons_cache')
-          ->removeColumn('ancestors_cache')
-          ->update();
+    if (!$table->hasColumn('treepath'))
+    {
+      $table->addColumn('treepath', 'string', ['null' => true])
+            ->update();
+    }
 
     $table = $this->table('documentcategories');
-    $table->removeColumn('level')
-          ->removeColumn('sons_cache')
-          ->removeColumn('ancestors_cache')
-          ->update();
+    if (!$table->hasColumn('treepath'))
+    {
+      $table->addColumn('treepath', 'string', ['null' => true])
+            ->update();
+    }
 
     $table = $this->table('ipnetworks');
-    $table->removeColumn('level')
-          ->removeColumn('sons_cache')
-          ->removeColumn('ancestors_cache')
-          ->update();
+    if (!$table->hasColumn('treepath'))
+    {
+      $table->addColumn('treepath', 'string', ['null' => true])
+            ->update();
+    }
+
+    $this->treepaths = [];
+    $this->generateTreepath('0', 'entities', 'entity_id');
+
+    $this->treepaths = [];
+    $this->generateTreepath('0', 'categories', 'category_id');
+
+    $this->treepaths = [];
+    $this->generateTreepath('0', 'groups', 'group_id');
+
+    $this->treepaths = [];
+    $this->generateTreepath('0', 'knowbaseitemcategories', 'knowbaseitemcategory_id');
+
+    $this->treepaths = [];
+    $this->generateTreepath('0', 'locations', 'location_id');
+
+    $this->treepaths = [];
+    $this->generateTreepath('0', 'softwarelicensetypes', 'softwarelicensetype_id');
+
+    $this->treepaths = [];
+    $this->generateTreepath('0', 'softwarecategories', 'softwarecategory_id');
+
+    $this->treepaths = [];
+    $this->generateTreepath('0', 'states', 'state_id');
+
+    $this->treepaths = [];
+    $this->generateTreepath('0', 'businesscriticities', 'businesscriticity_id');
+
+    $this->treepaths = [];
+    $this->generateTreepath('0', 'documentcategories', 'documentcategory_id');
+
+    $this->treepaths = [];
+    $this->generateTreepath('0', 'ipnetworks', 'ipnetwork_id');
+
+    $table = $this->table('entities');
+    if ($table->hasColumn('level'))
+    {
+      $table->removeColumn('level')
+            ->removeColumn('sons_cache')
+            ->removeColumn('ancestors_cache')
+            ->update();
+    }
+
+    $table = $this->table('groups');
+    if ($table->hasColumn('level'))
+    {
+      $table->removeColumn('level')
+            ->removeColumn('sons_cache')
+            ->removeColumn('ancestors_cache')
+            ->update();
+    }
+
+    $table = $this->table('knowbaseitemcategories');
+    if ($table->hasColumn('level'))
+    {
+      $table->removeColumn('level')
+            ->removeColumn('sons_cache')
+            ->removeColumn('ancestors_cache')
+            ->update();
+    }
+
+    $table = $this->table('locations');
+    if ($table->hasColumn('level'))
+    {
+      $table->removeColumn('level')
+            ->removeColumn('sons_cache')
+            ->removeColumn('ancestors_cache')
+            ->update();
+    }
+
+    $table = $this->table('softwarelicensetypes');
+    if ($table->hasColumn('level'))
+    {
+      $table->removeColumn('level')
+            ->removeColumn('sons_cache')
+            ->removeColumn('ancestors_cache')
+            ->update();
+    }
+
+    $table = $this->table('softwarecategories');
+    if ($table->hasColumn('level'))
+    {
+      $table->removeColumn('level')
+            ->removeColumn('sons_cache')
+            ->removeColumn('ancestors_cache')
+            ->update();
+    }
+
+    $table = $this->table('states');
+    if ($table->hasColumn('level'))
+    {
+      $table->removeColumn('level')
+            ->removeColumn('sons_cache')
+            ->removeColumn('ancestors_cache')
+            ->update();
+    }
+
+    $table = $this->table('businesscriticities');
+    if ($table->hasColumn('level'))
+    {
+      $table->removeColumn('level')
+            ->removeColumn('sons_cache')
+            ->removeColumn('ancestors_cache')
+            ->update();
+    }
+
+    $table = $this->table('documentcategories');
+    if ($table->hasColumn('level'))
+    {
+      $table->removeColumn('level')
+            ->removeColumn('sons_cache')
+            ->removeColumn('ancestors_cache')
+            ->update();
+    }
+
+    $table = $this->table('ipnetworks');
+    if ($table->hasColumn('level'))
+    {
+      $table->removeColumn('level')
+            ->removeColumn('sons_cache')
+            ->removeColumn('ancestors_cache')
+            ->update();
+    }
+  }
+
+  public function down()
+  {
+    $table = $this->table('entities');
+    if (!$table->hasColumn('level'))
+    {
+      $table->addColumn('level', 'integer', ['null' => false, 'default' => '0'])
+            ->addColumn('sons_cache', 'text', ['null' => true, 'limit' => MysqlAdapter::TEXT_LONG])
+            ->addColumn('ancestors_cache', 'text', ['null' => true, 'limit' => MysqlAdapter::TEXT_LONG])
+            ->update();
+    }
+
+    $table = $this->table('groups');
+    if (!$table->hasColumn('level'))
+    {
+      $table->addColumn('level', 'integer', ['null' => false, 'default' => '0'])
+            ->addColumn('sons_cache', 'text', ['null' => true, 'limit' => MysqlAdapter::TEXT_LONG])
+            ->addColumn('ancestors_cache', 'text', ['null' => true, 'limit' => MysqlAdapter::TEXT_LONG])
+            ->update();
+    }
+
+    $table = $this->table('knowbaseitemcategories');
+    if (!$table->hasColumn('level'))
+    {
+      $table->addColumn('level', 'integer', ['null' => false, 'default' => '0'])
+            ->addColumn('sons_cache', 'text', ['null' => true, 'limit' => MysqlAdapter::TEXT_LONG])
+            ->addColumn('ancestors_cache', 'text', ['null' => true, 'limit' => MysqlAdapter::TEXT_LONG])
+            ->update();
+    }
+
+    $table = $this->table('locations');
+    if (!$table->hasColumn('level'))
+    {
+      $table->addColumn('level', 'integer', ['null' => false, 'default' => '0'])
+            ->addColumn('sons_cache', 'text', ['null' => true, 'limit' => MysqlAdapter::TEXT_LONG])
+            ->addColumn('ancestors_cache', 'text', ['null' => true, 'limit' => MysqlAdapter::TEXT_LONG])
+            ->update();
+    }
+
+    $table = $this->table('softwarelicensetypes');
+    if (!$table->hasColumn('level'))
+    {
+      $table->addColumn('level', 'integer', ['null' => false, 'default' => '0'])
+            ->addColumn('sons_cache', 'text', ['null' => true, 'limit' => MysqlAdapter::TEXT_LONG])
+            ->addColumn('ancestors_cache', 'text', ['null' => true, 'limit' => MysqlAdapter::TEXT_LONG])
+            ->update();
+    }
+
+    $table = $this->table('softwarecategories');
+    if (!$table->hasColumn('level'))
+    {
+      $table->addColumn('level', 'integer', ['null' => false, 'default' => '0'])
+            ->addColumn('sons_cache', 'text', ['null' => true, 'limit' => MysqlAdapter::TEXT_LONG])
+            ->addColumn('ancestors_cache', 'text', ['null' => true, 'limit' => MysqlAdapter::TEXT_LONG])
+            ->update();
+    }
+
+    $table = $this->table('states');
+    if (!$table->hasColumn('level'))
+    {
+      $table->addColumn('level', 'integer', ['null' => false, 'default' => '0'])
+            ->addColumn('sons_cache', 'text', ['null' => true, 'limit' => MysqlAdapter::TEXT_LONG])
+            ->addColumn('ancestors_cache', 'text', ['null' => true, 'limit' => MysqlAdapter::TEXT_LONG])
+            ->update();
+    }
+
+    $table = $this->table('businesscriticities');
+    if (!$table->hasColumn('level'))
+    {
+      $table->addColumn('level', 'integer', ['null' => false, 'default' => '0'])
+            ->addColumn('sons_cache', 'text', ['null' => true, 'limit' => MysqlAdapter::TEXT_LONG])
+            ->addColumn('ancestors_cache', 'text', ['null' => true, 'limit' => MysqlAdapter::TEXT_LONG])
+            ->update();
+    }
+
+    $table = $this->table('documentcategories');
+    if (!$table->hasColumn('level'))
+    {
+      $table->addColumn('level', 'integer', ['null' => false, 'default' => '0'])
+            ->addColumn('sons_cache', 'text', ['null' => true, 'limit' => MysqlAdapter::TEXT_LONG])
+            ->addColumn('ancestors_cache', 'text', ['null' => true, 'limit' => MysqlAdapter::TEXT_LONG])
+            ->update();
+    }
+
+    $table = $this->table('ipnetworks');
+    if (!$table->hasColumn('level'))
+    {
+      $table->addColumn('level', 'integer', ['null' => false, 'default' => '0'])
+            ->addColumn('sons_cache', 'text', ['null' => true, 'limit' => MysqlAdapter::TEXT_LONG])
+            ->addColumn('ancestors_cache', 'text', ['null' => true, 'limit' => MysqlAdapter::TEXT_LONG])
+            ->save();
+    }
   }
 
   private function generateTreepath($id, $table, $foreignkey)
@@ -174,7 +327,6 @@ final class TreepathsMigration extends AbstractMigration
         $treepath = $this->treepaths[$item[$foreignkey]] . $treepath;
       }
       $this->treepaths[$item['id']] = $treepath;
-
       $builderUpdate = $this->getQueryBuilder('update');
       $builderUpdate
         ->update($table)
