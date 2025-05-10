e04091f5bd5f26640eb1dcbb5dee44c18385b032
David Durieux <david@durieux.family>
2025-05-10 10:13:53 +0200

    Fix lint

--- db/migrations/20250426132501_rename_rule_right.php
+++ db/migrations/20250426132501_rename_rule_right.php
@@ -11,12 +11,18 @@ final class RenameRuleRight extends AbstractMigration
   public function up(): void
   {
     $this->execute('UPDATE rules SET sub_type = ? WHERE sub_type = ?', ['user', 'RuleRight']);
-    $this->execute('UPDATE profilerights SET model = ? WHERE model = ?', ['App\Models\Rules\User', 'App\Models\Rules\Right']);
+    $this->execute(
+      'UPDATE profilerights SET model = ? WHERE model = ?',
+      ['App\Models\Rules\User', 'App\Models\Rules\Right']
+    );
   }
 
   public function down(): void
   {
     $this->execute('UPDATE rules SET sub_type = ? WHERE sub_type = ?', ['RuleRight', 'user']);
-    $this->execute('UPDATE profilerights SET model = ? WHERE model = ?', ['App\Models\Rules\Right', 'App\Models\Rules\User']);
+    $this->execute(
+      'UPDATE profilerights SET model = ? WHERE model = ?',
+      ['App\Models\Rules\Right', 'App\Models\Rules\User']
+    );
   }
 }
